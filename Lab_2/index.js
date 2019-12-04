544aeb76aea995fdd7afad76bad8bbcde4c07e85
Ilya Wise <ilyaphotoshop17@gmail.com>
2019-12-04 17:26:24 +0200

    [add] Added solutions to the second laboratory

--- Lab_2/index.js
+++ Lab_2/index.js
@@ -8,13 +8,30 @@ class Statistic {
         return console.log(result + add);
     }
 
+    searchMinimumMaximum(min, max) {
+        const date = [min, max];
+        console.log('Value: ' +date)
 
+        let maxNum = Math.max(...date);
+        console.log('maxNum: ' + maxNum)
+
+        let minNum = Math.min(...date);
+        console.log('minNum: ' + minNum)
+    }
+
+    searchAverage(number = []) {
+        const allValueAdd = number;
+        console.log(allValueAdd)   //nWork
+    }
 }
 
 const newStatistic = new Statistic();
-newStatistic.addResultExperiment(40, 10);
+// newStatistic.addResultExperiment(40, 10);
+// newStatistic.searchMinimumMaximum(10,20);
+newStatistic.searchAverage(12,32,42)
 
 // Lab_2_task_2
+
 class Dog {
     constructor(age, name) {
         this.name = name;
@@ -33,3 +50,56 @@ newDog.sayHi(1)
 
 
 //Lab_2_task_3
+
+class Task3 extends Dog {
+    constructor() {
+        super();
+        this.age = 0;
+        this.name = 'Незнайомець,';
+    }
+
+    task3NewNameAge(name, age) {
+        console.log('Собака ' + name + ' Вік ' + age);
+    }
+}
+
+const newTask3 = new Task3();
+newTask3.task3NewNameAge('Пугач', 21)
+
+
+//Lab_2_task_4
+
+class Task4 extends Task3 {
+    constructor() {
+        super(age, name);
+    }
+
+    calObject() {
+
+    }
+}
+
+
+
+//Lab_2_task_5
+
+
+class Detail {
+    saveDate(name, trademark, country, manufacturer, dateOfManufacture, uptime, comment) {
+
+        if (typeof name || typeof trademark || typeof country || typeof manufacturer || typeof dateOfManufacture || typeof uptime || typeof comment === undefined) {
+            console.error('Введите все данные');
+        }
+
+        console.log(`Имя ${name} Торгова Марка ${trademark} Страна ${country} Виробник ${manufacturer} Дата Виробництва ${dateOfManufacture} Час Безвідмовної Роботи ${uptime} Коментарий ${comment}`)
+
+        this.establishmentDate()
+    }
+
+    establishmentDate() {
+        console.log('Дата: ' +new Date())
+    }
+}
+
+const newDetail = new Detail();
+newDetail.saveDate('Ilya','РошенBadChoc')
